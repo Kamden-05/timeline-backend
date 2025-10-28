ce90a842b0ea3475a8aa0dd7db96c57230b94145
Kamden-05 <kamdenw05@gmail.com>
2025-10-28 18:38:06 -0500

    created root endpoint

--- src/main.py
+++ src/main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(timelines.router)
 app.include_router(notes.router)
+
+
+@app.get("/")
+def root():
+    return {'message': "Kam's Timeline Maker!"}
+
+@app.get("/health")
+def check_health():
+    return {"status: ok"}
