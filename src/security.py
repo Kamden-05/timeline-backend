d30d992af3e5d469614e58e9972dab7fc1c7ba5c
Kamden-05 <kamdenw05@gmail.com>
2025-10-28 19:16:34 -0500

    added method to get jwt token

--- src/security.py
+++ src/security.py
@@ -1,4 +1,14 @@
 from pwdlib import PasswordHash
+from datetime import timedelta, datetime, timezone
+from typing import Optional
+import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
 
 password_hash = PasswordHash.recommended()
 
@@ -7,3 +17,14 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password: str) -> str:
     return password_hash.hash(password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta]=None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
