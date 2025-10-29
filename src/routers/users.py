8ed6026c88868a1db62395300da8e126fca28445
Kamden-05 <kamdenw05@gmail.com>
2025-10-28 19:50:50 -0500

    started user endpoints

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,3 +1,29 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter()
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+
+from crud import user_crud
+from db import get_db
+from models.user_models import User, UserCreate, UserRead, UserUpdate
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+oauth2 = Annotated[str, oauth2_scheme]
+DbSession = Annotated[Session, Depends(get_db)]
+
+
+@router.get("/me", response_model=UserRead)
+def get_current_user(user_id: int, db: DbSession):
+    user = user_crud.get_user(db, user_id)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {user_id} not found",
+        )
+
+    return user
