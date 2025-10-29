5289eb0c7c63507a7de95a10125d6ec0047a53d4
Kamden-05 <kamdenw05@gmail.com>
2025-10-28 19:55:28 -0500

    created login and register routes

---
+++ src/routers/auth.py
@@ -0,0 +1,74 @@
+from datetime import timedelta
+from typing import Annotated, Optional
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from sqlmodel import Session
+from models.user_models import UserCreate, UserDbCreate
+from config import settings
+from crud import user_crud
+from db import get_db
+from security import create_access_token, get_password_hash
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+oauth2 = Annotated[str, oauth2_scheme]
+DbSession = Annotated[Session, Depends(get_db)]
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+@router.post("/login", response_model=Token)
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session):
+    user = user_crud.authenticate_user(
+        db, email=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.id}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.post("/register", response_model=Token)
+def register(user_create: UserCreate, db: DbSession):
+    user = user_crud.get_user_by_email(db, email=user_create.email)
+
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"A user with that email already exists",
+        )
+
+    db_user = UserDbCreate(
+        **user_create.model_dump(exclude_unset=True, exclude_defaults=True),
+        password_hash=get_password_hash(user_create.password),
+    )
+
+    new_user = user_crud.create_user(db, db_user)
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": new_user.id}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
